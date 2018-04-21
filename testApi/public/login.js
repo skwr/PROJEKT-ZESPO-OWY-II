871e646f3a60d290401fab225b62a45e06fb018f
Mateko <36420445+Mateko@users.noreply.github.com>
2018-04-21 18:55:47 +0200

    Add files via upload

---
+++ testApi/public/login.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $("#ok").click(function(){
+		$("#wrapper").fadeIn(1000);
+		$("#login").hide();
+	});
+	$("#submit").click(function()
+	{
+		var scrollowanko = $("#foo");
+	if
+	});
+	
+});
