c9bd35ed63f3b216f18b8fdcb6c4e4f106168f0d
Julian Skowroński <julian.skowronski@gmail.com>
2018-04-21 18:46:57 +0200

    dodanie w serverze funkcji logowania i wylogowania clienta

--- testApi/api.js
+++ testApi/api.js
@@ -11,11 +11,25 @@ var io = require('socket.io').listen(server);		//dolaczenie servera socket.io i 
 
 const port = 3000;	//okreslenie portu do nasluchiwania
 
-io.sockets.on('connection', onConnect);		//wywolanie funkcji onConnect po podlaczeniu dowolnego clienta
+io.on('connection', function(socket){
+	onConnect(socket);
+	
+	socket.on('disconnect', function () {
+    onDisconnect(socket);
+  });
+});
+
+//io.sockets.on('connection', onConnect);		//wywolanie funkcji onConnect po podlaczeniu dowolnego clienta
+
+//io.sockets.on('disconnect', onDisconnect);	//wywolanie funkcji onDisconnect po odlaczeniu dowolnego clienta
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));		//przypisanie parsera
 
 require('./index.js')(app, io);		//dołączenie index.js, ktore odpowiada za odbieranie żądan POST
+
+var users = [];
 
 server.listen(port, () => {
 	//wywolywane w momencie rozpoczecia nasluchiwania na porcie
@@ -31,7 +45,29 @@ app.get('/', function(req, res){
 function onConnect(socket)
 {	
 	//wywolywane w momencie podlaczenia dowolnego clienta do servera
-	console.log('client connected');
+	console.log('client connected ' + socket.id);
+	var elem = {"id":socket.id, "name":""};
+	users.push(elem);
+}
+
+function onDisconnect(socket)
+{
+	//wywolywane w momencie odlaczenia dowolnego clienta od servera
+	
+	var index = -1;
+	
+	var i = 0;
+	while(index < 0 && i < users.length)
+	{
+		if(users[i].id === socket.id)
+		{
+			index = i;
+		}
+		i++;
+	}
+	users.splice(index, 1);
+	
+	console.log('client disconnected ' + socket.id);
 }
 
 
