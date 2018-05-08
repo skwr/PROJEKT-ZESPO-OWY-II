3b6cc28be865a51c305b7484331a5330a6298cee
Patryk Chiliński <p.chilinski@gmail.com>
2018-05-08 19:18:27 +0200

    funkcja do zmiany stylów

---
+++ testApi/public/theme.js
@@ -0,0 +1,6 @@
+document.getElementById('light').onclick = function () { // zmiana stylu
+	document.getElementById('theme').href = 'style.css';
+};
+document.getElementById('dark').onclick = function () {
+	document.getElementById('theme').href = 'style2.css';
+};
