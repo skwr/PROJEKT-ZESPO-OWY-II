3fb6b496881d547dfa2834b443c83fb0dc6f1545
Mateko <mateuszpierzchala01@gmail.com>
2018-05-12 16:57:46 +0200

    drobne zmiany wygladu

--- testApi/public/login.js
+++ testApi/public/login.js
@@ -7,8 +7,10 @@ $( document ).ready(function() {
 		$("#czesc").fadeIn(2000);
 		$("#users").fadeIn(2000);
 		$("#koledzy").fadeIn(2000);
+		$("#nameff").fadeIn(2000);
 		$("#login").hide();
 		var nick = $("#nick").val();
+		$("#czesc").html("Witamy na czacie:  " + nick);
 		sendUserName(nick);
 		return false;
 	});
